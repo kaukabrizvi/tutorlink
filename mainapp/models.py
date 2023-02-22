c28186af9247282fd81cc518fd088039333a14eb
MooshKhan <82980854+MooshKhan@users.noreply.github.com>
2023-02-22 15:34:14 -0500

    Update models.py

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+class Profile(models.Model):
+        user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
 
-# Create your models here.
+
+        def str(self):
+                return str(self.user)
+
+        @receiver(post_save, sender=User)
+        def create_user_profile(sender, instance, created, kwargs):
+                if created:
+                        Profile.objects.create(user=instance)
+
+        @receiver(post_save, sender=User)
+        def save_user_profile(sender, instance, kwargs):
+                instance.profile.save()
