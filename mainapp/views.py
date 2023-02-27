d2119f5a019c8253482d71be4fdc052f689ba7a1
hunterbrownn <ghb6mt@virginia.edu>
2023-02-26 21:13:18 -0500

    Sign in Fix
    The sign in now shows the value of whether the user is a tutor or student

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,17 +3,23 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic
 from django.urls import reverse
 from django.shortcuts import redirect
+from .models import Profile
 # Create your views here.
 
 
 def index(request):
-    return render(request,"mainapp/landing_page.html")
+    user = Profile.objects.all()
+    context = {
+        "Profiles" : user
+    }
+    return render(request,"mainapp/landing_page.html",context)
 
 #Assigns user type
 def assignUserType(request):
     if "tutorbtn" in request.POST:
         theUser = request.user
-        theUser.is_tutor = True
+        print(theUser.username)
+        theUser
         theUser.save()
         print(theUser.is_tutor)
         return redirect('index')
