7d48e0487a5645178880a8229cc2e6bdd73de94e
hunterbrownn <ghb6mt@virginia.edu>
2023-02-26 22:14:40 -0500

    Imports fixed v2

--- mainapp/models.py
+++ mainapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+
 class Profile(models.Model):
         REQUIRED_FIELDS = ('user',)
         user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
