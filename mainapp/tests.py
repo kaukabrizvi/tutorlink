ece62ce0cc7f73b10161b376c23aa030394b9577
MooshKhan <82980854+MooshKhan@users.noreply.github.com>
2023-03-19 19:52:54 -0400

    Update tests.py

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.urls import reverse
+from django import setup
+import os
+from .sis_data import get_depts
 
-# Create your tests here.
+# Create tests here.
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+setup()
+
+class LandingPageTestCase(TestCase):
+  
+    def test_correct_home_page(self):
+      
+        response = self.client.post(reverse('index'))
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'tutor-link/landing_page.html')
+
+    def test_dept_list_pulled_correctly(self):
+      
+        depts = get_depts("https://tutor-link.herokuapp.com/api/")
+        
+        self.assertGreater(len(depts), 0)
+        self.assertTrue("APMA" in depts)
+        self.assertTrue("PHYS" in depts)
+
